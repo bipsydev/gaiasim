9aa4f5a56a5b89c7c52dd8c2a5e8aa03b86b09f2
Heather Crawford <bipsydev@gmail.com>
2026-05-10 13:38:42 -0500

    Add better comments

--- android_debug.sh
+++ android_debug.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+# This script will build, install, and run an Android APK using Gradle and adb.
+# Make sure that `android_init.sh` has been run first to set up the android-project directory and configuration.
+# Should automatically close when logcat detects that the app has terminated. (not still in background)
+
 # Change to the Gradle project directory
 cd android-project/ || exit 1
 
-# Build the APK
+# Build the APK (uses the build.gradle config we copied over in `android_init.sh`)
 echo "Building APK..."
 ./gradlew assembleDebug || exit 1
 
-# Install the APK
+# Install the APK (must connect a device with adb first)
 echo "Installing APK..."
 adb install -r ./app/build/outputs/apk/debug/app-debug.apk || (echo "APK install failed. Did you connect a device with adb yet?" && sleep 5 && exit 1)
 
 # Start the app
+# The package and activity name are hardcoded here, change if needed later!
 echo "Starting app..."
 adb shell am start -n org.libsdl.app/.SDLActivity || exit 1
 
-# Wait for app to fully start
+# Wait a sec for app to fully start
 echo "Waiting for app to start..."
 sleep 1
 
