a16bff38749de177a43cd9097aa25d02e9375c6b
Heather Crawford <bipsydev@gmail.com>
2026-05-10 13:10:47 -0500

    Remove android-project symlink, make init script copy template project

--- android_init.sh
+++ android_init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Clean and copy over the android-project starter template from SDL
+rm -rf android-project
+cp -r external_libs/SDL/android-project/ android-project
+
 # Create symlinks to libraries into the JNI directory so that the Android NDK can compile them
 # Create symlinks for all SDL libraries
 ln -srf -t android-project/app/jni/ external_libs/SDL*
