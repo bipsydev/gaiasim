b155dd11f4d95a7e34d3076f4965a881f7fbdd0a
Heather Crawford <bipsydev@gmail.com>
2026-05-10 11:08:38 -0500

    Create android_init.sh

---
+++ android_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create symlinks to libraries into the JNI directory so that the Android NDK can compile them
+# Create symlinks for all SDL libraries
+ln -sf -t android-project/app/jni/ external_libs/SDL*
+
+# copy build.gradle into android-project/app/ to replace it
+cp build.gradle android-project/app/build.gradle
