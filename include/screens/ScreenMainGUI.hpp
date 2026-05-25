d3dc619a93dc9182d61935e8b22d7d5a07a0884e
Heather Crawford <bipsydev@gmail.com>
2026-05-25 10:46:38 -0500

    Refactor member names for consistency

--- include/screens/ScreenMainGUI.hpp
+++ include/screens/ScreenMainGUI.hpp
@@ -37,8 +37,8 @@ class ScreenMainGUI
   Panel m_top_sidebar;
 
   // Our cached window dimensions
-  int window_width;
-  int window_height;
+  int m_window_width;
+  int m_window_height;
 
 
 public:
@@ -46,7 +46,7 @@ public:
   : m_main_panel({0.25, 0.25, 0.75, 0.75}, "Main", {218, 255, 0, 255}),
     m_left_sidebar({0, 0.25, 0.25, 0.75}, "Left Sidebar", {255, 0, 203, 255}),
     m_top_sidebar({0, 0, 1.0, 0.25}, "Top Sidebar", {0, 180, 93, 255}),
-    window_width{0}, window_height{0}
+    m_window_width{0}, m_window_height{0}
   {
   }
   ~ScreenMainGUI() { };
@@ -76,8 +76,8 @@ private:
     log_info(std::format(
       "ScreenMainGUI::update_layout({}, {}) called.",
       window_width, window_height));
-    this->window_width = window_width;
-    this->window_height = window_height;
+    this->m_window_width = window_width;
+    this->m_window_height = window_height;
   }
 
   SDL_AppResult render_panel(SDL_Renderer *renderer, const Panel &panel)
@@ -89,10 +89,10 @@ private:
     // Convert panel partition to actual pixel bounds
     SDL_FRect m_bounds;
     // multiply partition values by window dimensions to get pixel bounds
-    m_bounds.x = panel.m_partition.x * window_width;
-    m_bounds.y = panel.m_partition.y * window_height;
-    m_bounds.w = panel.m_partition.w * window_width;
-    m_bounds.h = panel.m_partition.h * window_height;
+    m_bounds.x = panel.m_partition.x * m_window_width;
+    m_bounds.y = panel.m_partition.y * m_window_height;
+    m_bounds.w = panel.m_partition.w * m_window_width;
+    m_bounds.h = panel.m_partition.h * m_window_height;
     if (not SDL_RenderFillRect(renderer, &m_bounds))
     {
       return log_error(std::format(
